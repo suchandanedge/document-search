412b48c3162455dd52e756a8350ad8c87a84b578
suchandan.mondal@edgewrapper.com <suchandan.mondal@edgewrapper.com>
2025-05-27 21:05:17 +0000

    Edited ServerModule1, documentSearch.document_search_platform.document_search_type and documentSearch.document_search_platform.document_search_type.document_search_line_items

--- client_code/documentSearch/document_search_platform/document_search_type/__init__.py
+++ client_code/documentSearch/document_search_platform/document_search_type/__init__.py
@@ -6,5 +6,21 @@ class document_search_type(document_search_typeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    db_data_records = self.item['records']
     self.label_doc_type.text = self.item['doc_type_name']
-    self.repeating_panel_records.items = self.item['records']
+    self.repeating_panel_records.items = db_data_records
+    item_count = len(self.item['records'])
+    self.item_count.text = item_count
+    total_ship_qty = sum(
+      int(row.get("ship_qty", 0)) if str(row.get("ship_qty", "")).isdigit() else 0
+      for row in db_data_records
+    )
+    self.total_ship_qty.text = total_ship_qty
+
+  def toggle_button_click(self, **event_args):
+    if self.toggle_button.icon == 'fa:angle-right':
+      self.toggle_button.icon = 'fa:angle-down'
+      self.column_panel_1.visible = True
+    else:
+      self.toggle_button.icon = 'fa:angle-right'
+      self.column_panel_1.visible = False
