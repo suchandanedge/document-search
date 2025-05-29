936691c10b2e91dcc0a502092151d77360eac781
suchandan.mondal@edgewrapper.com <suchandan.mondal@edgewrapper.com>
2025-05-29 15:08:19 +0000

    Edited documentSearch.document_search_platform.document_search_type and documentSearch.document_search_platform.document_search_type.RowTemplate1; Deleted documentSearch.document_search_platform.document_search_type.RowTemplate1.document_search_line_items

--- client_code/documentSearch/document_search_platform/document_search_type/__init__.py
+++ client_code/documentSearch/document_search_platform/document_search_type/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import document_search_typeTemplate
 from anvil import *
 import anvil.server
+import json
 
 class document_search_type(document_search_typeTemplate):
   def __init__(self, **properties):
@@ -9,8 +10,21 @@ class document_search_type(document_search_typeTemplate):
     db_data_records = self.item['records']
     self.label_doc_type.text = self.item['doc_id']
     # self.repeating_panel_records.items = db_data_records
-    self.repeating_panel_1.items = db_data_records
-    print(self.item)
+    flattened_records = []
+    for record in db_data_records:
+      raw_payload = record.get('payload', '{}')
+      payload = json.loads(raw_payload)
+      payload['item_name'] = record.get('item_name', '')
+      flattened_records.append(payload)
+    print(flattened_records)
+    self.data_grid_1.columns = [
+      {"id": "item_name", "title": "Item Name", "data_key": "item_name"},
+      {"id": "customer", "title": "Customer", "data_key": "customer"},
+      {"id": "warehouse", "title": "Warehouse", "data_key": "warehouse"},
+      {"id": "inwardReference", "title": "Inward Ref", "data_key": "inwardReference"},
+      {"id": "inwardLines", "title": "Inward Lines", "data_key": "inwardLines"},
+    ]
+    self.repeating_panel_records.items = flattened_records
     self.date.text= self.item['records'][0]['log_date']
 
   def toggle_button_click(self, **event_args):
